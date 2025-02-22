c25131b720de36392419387c392158b3af27eeb7
HoangHT2004 <tuanhoang112004@gmail.com>
2025-02-22 12:13:19 +0700

    token

--- BE/skincare_booking/src/main/java/com/seroter/skincare_booking/service/AuthenticationService.java
+++ BE/skincare_booking/src/main/java/com/seroter/skincare_booking/service/AuthenticationService.java
@@ -4,20 +4,24 @@ import com.seroter.skincare_booking.entity.Account;
 import com.seroter.skincare_booking.exception.DuplicateEntity;
 
 import com.seroter.skincare_booking.repository.AuthenticationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
-    private final AuthenticationRepository authenticationRepository;
 
-    public AuthenticationService(AuthenticationRepository authenticationRepository) {
-        this.authenticationRepository = authenticationRepository;
-    }
+    @Autowired
+    AuthenticationRepository authenticationRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public Account register(Account account) {
         //Xử lý logic
 
         //lưu xuống database
+
         Account newAccount = authenticationRepository.save(account);
         return newAccount;
     }

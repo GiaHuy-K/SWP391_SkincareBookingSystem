a8c5a1197f6c8f351b8116a11080a9e66da9311e
Gia Huy <chutran01012004@gmail.com>
2025-03-01 10:24:33 +0700

    admin and manage

---
+++ FE/FE_SWP391_Skincare/src/components/templates/dashboardTemplate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardTemplate() {
+  return (
+    <div>DashboardTemplate</div>
+  )
+}
+
+export default DashboardTemplate
--- FE/FE_SWP391_Skincare/src/main.jsx
+++ FE/FE_SWP391_Skincare/src/main.jsx
@@ -5,6 +5,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login/index.jsx";
 import RegisterPage from "./pages/register/index.jsx";
 import { ToastContainer } from "react-toastify";
+
+import ManageBooking from "./pages/admin/manage-booking.jsx";
+import ManageUser from "./pages/admin/manage-user.jsx";
+import AdminLayout from "./components/layouts/adminLayout.jsx";
+
 <import>RouterProvider,</import>;
 
 const router = createBrowserRouter([
@@ -15,6 +20,20 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <AdminLayout/>,
+    children:[
+      {
+        path: "/dashboard/booking",
+        element: <ManageBooking/>
+      },
+      {
+        path: "/dashboard/user",
+        element: <ManageUser/>
+      },
+    ]
   },
 ]);
 

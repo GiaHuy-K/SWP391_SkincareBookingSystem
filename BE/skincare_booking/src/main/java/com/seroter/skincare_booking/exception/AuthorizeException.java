feb166db944635773961756bb0b66065d4d7289b
HoangHT2004 <tuanhoang112004@gmail.com>
2025-03-01 15:15:54 +0700

    FilterNew

---
+++ BE/skincare_booking/src/main/java/com/seroter/skincare_booking/exception/AuthorizeException.java
@@ -0,0 +1,7 @@
+package com.seroter.skincare_booking.exception;
+
+public class AuthorizeException extends RuntimeException {
+  public AuthorizeException(String message) {
+    super(message);
+  }
+}

8bedb6aae774610b6b084b0e1da349edb8993a32
caoduy13 <caoduy856@gmail.com>
2025-03-05 17:06:49 +0700

    merge main into duy branch

--- BE/skincare_booking/src/main/java/com/seroter/skincare_booking/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seroter.skincare_booking.repository;
-
-import com.seroter.skincare_booking.entity.SpaService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<SpaService, Long> {
-}

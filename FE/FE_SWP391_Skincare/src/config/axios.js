b9c6188ff3fecb42464061c90fae744251d53bc3
Gia Huy <chutran01012004@gmail.com>
2025-03-05 10:41:24 +0700

    add adminlayout plain

--- FE/FE_SWP391_Skincare/src/config/axios.js
+++ FE/FE_SWP391_Skincare/src/config/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'http://222.255.117.200:8080/api'
+    // baseURL: 'http://localhost:8080/api'
 });
 api.interceptors.request.use(
     function (config) {

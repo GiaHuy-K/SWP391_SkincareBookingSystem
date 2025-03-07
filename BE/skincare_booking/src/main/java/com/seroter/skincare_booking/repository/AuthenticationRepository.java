bc12f08dd8659a59dbb1d7ee885dba3fa7687f13
HoangHT2004 <tuanhoang112004@gmail.com>
2025-03-07 14:57:12 +0700

    List Role, SkincareServiceResponse, Fix AdminAPI

--- BE/skincare_booking/src/main/java/com/seroter/skincare_booking/repository/AuthenticationRepository.java
+++ BE/skincare_booking/src/main/java/com/seroter/skincare_booking/repository/AuthenticationRepository.java
@@ -1,12 +1,16 @@
 package com.seroter.skincare_booking.repository;
 
 import com.seroter.skincare_booking.entity.Account;
+import com.seroter.skincare_booking.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuthenticationRepository extends JpaRepository<Account, Long> {
     Account findById(long id);
 
     Optional<Account> findByUsername(String username);
+
+    List<Account> findAllByRoleEnum(RoleEnum roleEnum);
 }
